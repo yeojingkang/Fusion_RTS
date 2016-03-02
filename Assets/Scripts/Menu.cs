e988d8e219ac9a9d7b3f1b68b70cbfdde80fca12
Toh Yu Xuan <esson_96@hotmail.com>
2016-03-02 19:10:51 +0800

    added menu and im done thanks guys

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Net;
+public class Menu : MonoBehaviour {
+
+    public Text playerIP;
+
+    void Start()
+    {
+        playerIP.text = "IP Address: "+TestIP();
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    string TestIP()
+    {
+        return Network.player.ipAddress;
+    }
+
+
+}

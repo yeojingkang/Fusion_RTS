baa31521ea81147b344baec700c8336a50f6d33f
Yeo Jing Kang <yeojingkang@hotmail.com>
2016-03-04 00:38:02 +0800

    Improved selection box

--- Assets/Scripts/ObjectControls.cs
+++ Assets/Scripts/ObjectControls.cs
@@ -9,9 +9,6 @@ public class ObjectControls : MonoBehaviour {
 
 	List<Object>		selected_units = new List<Object>();
 
-	bool				update_select_box_max_x = true;
-	bool				update_select_box_max_y = true;
-
 	// Use this for initialization
 	void Start () {
 
@@ -25,21 +22,7 @@ public class ObjectControls : MonoBehaviour {
 		}
 
 		if(Input.GetMouseButton(0)) {
-			update_select_box_max_x = Input.mousePosition.x > selection_box.xMin;
-			update_select_box_max_y = Input.mousePosition.y > selection_box.yMin;
-
-			if (update_select_box_max_x)
-				selection_box.xMax = Input.mousePosition.x;
-			else
-				selection_box.xMin = Input.mousePosition.x;
-
-			if (update_select_box_max_y)
-				selection_box.yMax = Input.mousePosition.y;
-			else
-				selection_box.yMin = Input.mousePosition.y;
-
-
-			//selection_box.max = Input.mousePosition;
+			selection_box.max = Input.mousePosition;
 		}
 
 		if(Input.GetMouseButtonUp(0)) {
@@ -50,8 +33,7 @@ public class ObjectControls : MonoBehaviour {
 			foreach(Transform unit in owned_units.transform) {
 				Vector3 unitScreenPos = camera.WorldToScreenPoint(unit.position);
 
-				if(unitScreenPos.x > selection_box.xMin && unitScreenPos.x < selection_box.xMax
-				&& unitScreenPos.y > selection_box.yMin && unitScreenPos.y < selection_box.yMax) {
+				if(selection_box.Contains(unitScreenPos, true)) {
 					selected_units.Add(unit.GetComponent<Object>());
 				}
 			}

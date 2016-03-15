7910bfb597e70ef18825d4c4c5a3b871d5bda699
Seo Wee Kuan <exswk02@gmail.com>
2016-03-15 18:36:44 +0800

    Added a simple Fog Of War into the game scene. FogOfWarPlane acts as the "texture" covering the map Added a FogOfWar shader and material, you can modify fog of war radius from the material Added a new layer specifically for fog of war Entities that is affected by the fog of war should be attached with a FogOfWarEntity script to work

---
+++ Assets/Scripts/FogOfWarEntity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogOfWarEntity : MonoBehaviour {
+
+	private int m_LayerMask;
+	public Transform m_FogOfWarPlane;
+
+	// Use this for initialization
+	void Start () {
+		m_LayerMask = (int)(1 << 9);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 screenPos = Camera.main.WorldToScreenPoint (transform.position);
+		Ray rayToPlayerPos = Camera.main.ScreenPointToRay (screenPos);
+
+		RaycastHit hit;
+		if (Physics.Raycast (rayToPlayerPos, out hit, 1000, m_LayerMask)) 
+		{
+			m_FogOfWarPlane.GetComponent<Renderer> ().material.SetVector ("_EntityPos", hit.point);
+		}
+	}
+}

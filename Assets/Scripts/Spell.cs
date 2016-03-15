66f2e9997f8c42255f004f62c026259ffee3f231
Yeo Jing Kang <yeojingkang@hotmail.com>
2016-03-15 17:00:51 +0800

    - Player can now fire first spell (Q + left-click on somewhere) - Commands now hold id of activated command (attack move, targeted spell) - Added cast range and cooldown for spells - Reduced stoppingDistance of unit NavMeshAgent - Add MoveCastSpell command for units (move towards target position if it is out of spell's cast range)

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -7,24 +7,44 @@ public class Spell : MonoBehaviour {
 		SPELL_NORMAL_ATTACK
 	}
 
+	GameObject spell = (GameObject)Resources.Load("Spells/Normal_Attack");
+
 	SpellType	type = SpellType.SPELL_NONE;
+	float		ori_cooldown = 0.0f;
 	float		cooldown = 0.0f;
+	float		cast_range = 0.0f;
 	bool		mustBeTargeted = false;
 
 	// Use this for initialization
 	void Start () {
-		type = SpellType.SPELL_NONE;
-		cooldown = 0.0f;
-		mustBeTargeted = false;
 	}
 
 	// Update is called once per frame
-	void Update () {
+	public void Update () {
 		if (cooldown > 0.0f)
 			cooldown -= Time.deltaTime;
+
+		if (cooldown < 0.0f)
+			cooldown = 0.0f;
 	}
 
-	public bool Cast() {
+	public void Init(SpellType newType) {
+		type = newType;
+
+		switch(type) {
+		case SpellType.SPELL_NONE:
+			break;
+
+		case SpellType.SPELL_NORMAL_ATTACK:
+			cast_range = 5.0f;
+			ori_cooldown = 3.0f;
+			mustBeTargeted = true;
+			spell = (GameObject)Resources.Load("Spells/Normal_Attack");
+			break;
+		}
+	}
+
+	public bool Cast(Vector3 position, Vector3 forward = default(Vector3), Quaternion rotation = default(Quaternion)) {
 		if (cooldown > 0.0f)
 			return false;
 
@@ -33,12 +53,16 @@ public class Spell : MonoBehaviour {
 			break;
 
 		case SpellType.SPELL_NORMAL_ATTACK:
+			Instantiate(spell, position + forward * 1.5f, rotation);
 			break;
 		}
 
+		cooldown = ori_cooldown;
 		return true;
 	}
 
 	public float	getCooldown() { return cooldown; }
+	public bool		isReady() { return cooldown <= 0.0f; }
 	public bool		isTargeted() { return mustBeTargeted; }
+	public float	getCastRange() { return cast_range; }
 }

d7d5c4bf9615a89f84444245ddce3fa34f18e579
Seo Wee Kuan <exswk02@gmail.com>
2016-03-23 22:32:10 +0800

    Added objective movement codes, Objective uses a sphere collider to detection units Based on the units, it will calculate a direction for the objective to move towards to I've also create a unit tag just for a easier check.

---
+++ Assets/Scripts/ObjectiveTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectiveTrigger : MonoBehaviour 
+{
+	public List<Transform> m_unitList = new List<Transform>();
+
+	void OnTriggerEnter(Collider col)
+	{
+		//Check if col is a unit
+		if (col.tag == "Unit") 
+		{
+			//Just in case, check if there already is an instance of it in the list
+			if (!m_unitList.Contains (col.transform)) 
+			{
+				m_unitList.Add (col.transform);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		m_unitList.Remove (col.transform);
+	}
+}
